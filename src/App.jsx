e19d299978871b3c9c23d6d75ed4bef425e59281
khalifa5000 <m.khalifa5000@gmail.com>
2024-06-17 23:11:06 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="card">
+        
+        <button >
+          count is 0
+        </button>
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
